9d2c8675eacb3031f5a17218fe613bb83e05d09c
Danrley Alves <danrleyalves.ufrn@gmail.com>
2023-09-12 18:14:40 -0300

    Create Trabalho 16.js

---
+++ Javascript e POO /Trabalho 16.js
@@ -0,0 +1,10 @@
+const funcionarios = 
+[
+    ['Nome', 'Idade', 'Salário', 'Tem ensino médio'],
+    ['José', 19, 1900, true],
+    ['David', 22, 2500, false],
+    ['Maria', 25, 3400, true],
+    ['Igor', 18, 2100, false]
+]
+
+console.table(funcionarios)
