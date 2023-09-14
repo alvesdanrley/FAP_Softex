e2772ca3ef88b930169b4d8b98ede98d86da6954
Danrley Alves <danrleyalves.ufrn@gmail.com>
2023-09-14 16:53:23 -0300

    Create Trabalho 17.js

---
+++ Javascript e POO /Trabalho 17.js
@@ -0,0 +1,9 @@
+// Para manipulação de Strings, podemos usar os métodos .toLowerCase() e .toUpperCase() que retornarão a cadeia de caracteres em letras minúsculas e maisculas, respectivamente
+
+// Exemplo
+
+const minusculas = 'abajur'
+const maiusculas = 'CARCAJU'
+
+console.log(minusculas.toUpperCase())
+console.log(maiusculas.toLowerCase())
