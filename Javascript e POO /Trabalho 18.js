d371fc87952b72793b9b2029bc3e473137487b0f
Danrley Alves <danrleyalves.ufrn@gmail.com>
2023-09-14 17:06:52 -0300

    Create Trabalho 18.js

---
+++ Javascript e POO /Trabalho 18.js
@@ -0,0 +1,12 @@
+//Instanciando um novo map
+const participantes = new Map()
+
+// Adicionando informações ao Map
+participantes.set('João', 50)
+participantes.set('Pedro', 120)
+participantes.set('Luiza', 70)
+participantes.set('Marcela', 10)
+participantes.set('Yuri', 34)
+
+// Imprimindo o Map na tela
+console.log(participantes)
